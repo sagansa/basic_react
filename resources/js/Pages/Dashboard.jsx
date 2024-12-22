c1023da0377c7a9f9a532e07acaf8d260603ed68
sagansa <83310652+sagansa@users.noreply.github.com>
2024-12-22 10:03:46 +0700

    Update Dashboard.jsx

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { Transition } from '@headlessui/react';
 
 export default function Dashboard() {
     return (
